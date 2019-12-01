bd604ecb03affac420517005be6102f09d5fad85
unknown <bruno_cunha@usp.br>
2019-12-01 16:10:18 -0300

    Time

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 class Main {
 
+    static long part1 = 0;
+    static long part2 = 0;
+    static long part3 = 0;
+
     public static String[] words = new String[36242];
 
     public static void part1() {
@@ -9,18 +13,49 @@ class Main {
             ReadersPriority proporcao = new ReadersPriority(words, i, 100-i);
             
             for(int j = 0; j < 50; j++) {
+                proporcao.bake();
+                long p = System.currentTimeMillis();
                 proporcao.execute();
+                p = System.currentTimeMillis() - p;
+                part1 += p;
             }
+
+            part1 /= 50;
+            System.out.println("Prioridade para leitores. Leitores:" + i + " escritores:" + (100-i)  + " média de tempo:" + part1);
         }
     }
 
     public static void part2() {
         for(int i = 0; i < 100; i++) {
+            WritersPriority proporcao = new WritersPriority(words, i, 100-i);
+            
+            for(int j = 0; j < 50; j++) {
+                proporcao.bake();
+                long p = System.currentTimeMillis();
+                proporcao.execute();
+                p = System.currentTimeMillis() - p;
+                part2 += p;
+            }
+
+            part2 /= 50;
+            System.out.println("Prioridade para escritores. Leitores:" + i + " escritores:" + (100-i) + " média de tempo:" + part2);
+        }
+    }
+
+    public static void part3() {
+        for(int i = 0; i < 100; i++) {
             UniqueAccess proporcao = new UniqueAccess(words, i, 100-i);
             
             for(int j = 0; j < 50; j++) {
+                proporcao.bake();
+                long p = System.currentTimeMillis();
                 proporcao.execute();
+                p = System.currentTimeMillis() - p;
+                part3 += p;
             }
+
+            part3 /= 50;
+            System.out.println("Prioridade para escritores. Leitores:" + i + " escritores:" + (100-i) + " média de tempo:" + part3);
         }
     }
 
@@ -45,7 +80,7 @@ class Main {
         }
 
         part1();
-
         part2();
+        part3();
     }
 }
