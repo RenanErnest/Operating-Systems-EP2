59143ba0de8ad2aab11c123ba4f26d7caca0b06d
Renan <51809247+RenanErnest@users.noreply.github.com>
2019-12-01 11:53:59 -0300

    Adjusts

--- Main.java
+++ Main.java
@@ -3,50 +3,118 @@ import java.util.Random;
 import java.util.concurrent.Semaphore;
 import java.util.*;
 
-class Reader extends Thread implements Common{
-    boolean isReader = true;
-    public void run(String[] words) {
+
+class CriticalRegion {
+    String[] words;
+    int readers = 0;
+
+    public CriticalRegion(String[] words) {
+        this.words = words;
+    }
+
+    public String[] read() {
+        synchronized(this)
+        {
+            this.readers++;
+        }
+        
         String[] read = new String[100];
         Random rand = new Random();
         for(int i = 0; i < 100; i++) {
             int j = rand.nextInt(words.length);
             read[i] = words[j];
         }
+
+        //DELAY
         try{
-            Thread.sleep (1);
+            Thread.sleep (1000);
         }
         catch (InterruptedException e){}
+     
+        synchronized(this)
+        {
+            this.readers--;
+            if (this.readers == 0)
+            {
+            this.notifyAll();
+            }
+        }
+        return read;
     }
-    public boolean isReader() { return isReader;}
-}
 
-class Writer extends Thread implements Common{
-    boolean isReader = false;
-    public void run(String[] words) {
+    public synchronized void write()
+    {
+        while (this.readers != 0) {
+            try {
+                this.wait();
+            }
+            catch (InterruptedException e) {}
+        }
+
         Random rand = new Random();
         for(int i = 0; i < 100; i++) {
             int j = rand.nextInt(words.length);
             words[j] = "MODIFICADO";
         }
+
+        //DELAY
         try{
-            Thread.sleep (1);
+            Thread.sleep (1000);
         }
         catch (InterruptedException e){}
+
+        this.notifyAll();
+    }
+}
+
+class Reader extends Thread implements Common{
+    boolean isReader = true;
+    CriticalRegion crit;
+
+    public Reader(CriticalRegion crit) {
+        this.crit = crit;
+    }
+
+    public void run() {
+        String[] read = crit.read();
+        System.out.println("A thread " + this.getName() + " leu");
+    }
+
+    public boolean isReader() { return isReader;}
+}
+
+class Writer extends Thread implements Common{
+    boolean isReader = false;
+    CriticalRegion crit;
+
+    public Writer(CriticalRegion crit) {
+        this.crit = crit;
+    }
+
+    public void run() {
+        crit.write();
+        System.out.println("A thread " + this.getName() + " modificou");
     }
     public boolean isReader() { return isReader;}
+}
+
+interface Common
+{
+    public boolean isReader();
+    public String getName();
+    public void start();
 }
 
 class Main{
 
     public static String[] words = new String[36242];
     public static Common[] readers_writers = new Common[100];
-    public static Semaphore mutex = new Semaphore(1);
-    public static Semaphore wrt = new Semaphore(1);
-    public static int readcnt;
+    public static CriticalRegion crit = new CriticalRegion(words);
     public static int readers_num = 50;
     public static int writers_num = 50;
 
     public static void main(String[] args) {
+        //Leitura de arquivo
         try{
             FileReader arq = new FileReader("bd.txt");
             BufferedReader reader = new BufferedReader(arq);
@@ -64,98 +132,38 @@ class Main{
         catch(Exception e) {
             System.out.println("Não foi possível encontrar o arquivo 'bd.txt'");
         }
-        for(int i = 0; i < words.length; i++) {
-            System.out.print(words[i] + " ");
-            if (i%15 == 0) System.out.println();
-        }
 
-        //importa collections
+
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        Collections.shuffle(numbers);
-
         for (int i = 0; i < 100; i++) 
         {
             numbers.add(new Integer(i)); 
         }
+        Collections.shuffle(numbers);
         
         for(int k = 0; k < readers_num; k++) // criar readers_num readers
         {
-            readers_writers[numbers.get(k)] = new Reader();
+            readers_writers[numbers.get(k)] = new Reader(crit);
         }
 
         for(int k = readers_num; k < readers_num + writers_num; k++) // criar writers_num writers (começa no readers_num e vai até readers_num + writers_num: 100)
         {
-            readers_writers[numbers.get(k)] = new Writer();
+            readers_writers[numbers.get(k)] = new Writer(crit);
         }
 
-
-        //a gente tem que inicializar o semaforo, mas ele leva um parametro que eu n entendi muito ainda
-        //https://www.geeksforgeeks.org/semaphore-in-java/
-        //acho que o mutex é 1, o outro eu n sei, parece que é 1 tbm, bora testar kkkk
-        for(int i = 0; i < 100 ; i ++)
+        for(int i = 0; i < 100 ; i++)
         {
             if(readers_writers[i].isReader() == false){
-                //is writer
-                do {
-                    // writer requests for critical section
-                    try { wrt.acquire();  } catch(Exception e) { System.out.println("A thread " + readers_writers[i].getName() + " foi interrompida"); } 
-                    // performs the write
-                    readers_writers[i].run(words);
-                    // leaves the critical section
-                    wrt.release();
-                } while(true);
+                System.out.println("Iniciando write num " + i + " " + readers_writers[i].getName());
+                readers_writers[i].start();
             }
             else{
-                //isso se reader
-                do {
-        
-                    // Reader wants to enter the critical section
-                    try { mutex.acquire();  } catch(Exception e) { System.out.println("A thread " + readers_writers[i].getName() + " foi interrompida"); }
-                
-                    // The number of readers has now increased by 1
-                    readcnt++;                          
-                
-                    // there is atleast one reader in the critical section
-                    // this ensure no writer can enter if there is even one reader
-                    // thus we give preference to readers here
-                    if (readcnt==1)  
-                    try { wrt.acquire();  } catch(Exception e) { System.out.println("A thread " + readers_writers[i].getName() + " foi interrompida"); }              
-                
-                    // other readers can enter while this current reader is inside 
-                    // the critical section
-                    mutex.release();           
-                
-                    // current reader performs reading here
-                    readers_writers[i].run(words);
-                    
-                    // a reader wants to leave 
-                    try { mutex.acquire();  } catch(Exception e) { System.out.println("A thread " + readers_writers[i].getName() + " foi interrompida"); }                
-                
-                    readcnt--;
-                
-                    // that is, no reader is left in the critical section,
-                    if (readcnt == 0) 
-                        wrt.release();         // writers can enter
-
-                    mutex.release(); // reader leaves
-                }
-                    while(true);
+                System.out.println("Iniciando read num " + i + " " + readers_writers[i].getName());
+                readers_writers[i].start();
             }
         }
         
     }
-
-    //https://www.geeksforgeeks.org/readers-writers-problem-set-1-introduction-and-readers-preference-solution/
-    //usar esse algoritmo aqui, pode ser?
-
-    //fechar o leitor de arquivo, eu n lembro, simples assim kkk kkkkkk
-    
     
 }
 
-interface Common
-{
-    public boolean isReader();
-    public void run(String[] words);
-    public String getName();
-}
